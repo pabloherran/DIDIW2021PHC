fdbd00a41763a3afeb75fb2b81f2bf6236e2f9d6
pabloherran <pabloherrancastrillo@gmail.com>
2021-04-14 17:17:03 +0200

    css

---
+++ Workspace/EjerciciosCSS/src/flowpanecss/MainFlowPaneCSS.java
@@ -0,0 +1,33 @@
+package flowpanecss;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class MainFlowPaneCSS extends Application {
+	private FlowPane rootLayout;
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// Carga el diseño del archivo FXML en la variable rootLayout
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainFlowPaneCSS.class.getResource("FlowPaneLayout.fxml"));
+			rootLayout = (FlowPane) loader.load();
+			rootLayout.getStylesheets().add("flowpanecss/ejercicio1.css");
+
+			// Mostramos la escena del FlowPane de la variable rootLayot
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ Workspace/EjerciciosCSS/src/module-info.java
@@ -0,0 +1,7 @@
+module EjerciciosCSS {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
